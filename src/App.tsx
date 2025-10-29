aee98ede9989e29caad89af6261ec0d26ca87629
Victor Pinto <victorpinto2304@gmail.com>
2025-10-29 17:37:22 -0500

    Change all project to React Typescript Vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
-export default function App() {
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-amber-50 text-gray-800 font-sans">
+    <div className="min-h-screen flex flex-col items-center justify-center bg-amber-50 text-gray-800 font-sans w-svw">
       <h1 className="text-4xl font-bold text-amber-700">🍪 Bienvenidos a Galletas Dulzura</h1>
       <p className="mt-4 text-lg">Las galletas más deliciosas hechas con amor 💛</p>
       <button className="mt-6 px-4 py-2 bg-amber-600 text-white rounded-lg hover:bg-amber-700">
@@ -9,3 +14,5 @@ export default function App() {
     </div>
   );
 }
+
+export default App
